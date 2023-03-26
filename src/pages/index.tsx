64bcbcd04317516505a63e60e03913647ca08a31
Vlad Sapozhnikov <me@lastw.net>
2023-03-26 21:43:17 +0700

    удалил awesome

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import { NextPage } from 'next';
-import Link from 'next/link';
 
 const IndexPage: NextPage = () => {
   return (
     <>
       <footer>
-        <Link href="/awesome">
-          <a>awesome</a>
-        </Link>
-        <span>|</span>
         <a href="https://twitter.com/lastwx">twitter</a>
         <a href="https://github.com/lastw">github</a>
       </footer>
