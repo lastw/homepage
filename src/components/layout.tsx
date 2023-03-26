b6b5a71d6347d6a6e1b00c8bb0918d9d3c759eaa
Vlad Sapozhnikov <me@lastw.net>
2023-03-26 21:57:57 +0700

    как обычно, спустя 3 года ничего не собирается, пока не обновишь пакеты

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 
-export const Layout: React.FC = ({ children }) => {
+export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="root">
       <canvas id="background" />
